d2377bb4a5c779cc8491fc9652a0ea34702648e2
ddddan <d1mullin@gmail.com>
2015-08-14 16:09:11 -0400

    v0.1.p1: Fork (personal)

--- get.php
+++ get.php
@@ -14,11 +14,7 @@ date_default_timezone_set('America/Toronto');
 error_reporting(E_ALL);
 
 $data = array(); // Will contain the final data set
-// TODO: Add the following parameters:
-// uid
-// allData - not just filtered data
-//
-// Read user data (if applicable)
+
 $user_info = explode('\\', filter_input(INPUT_SERVER, 'REMOTE_USER'));
 $user = end($user_info);
 
@@ -50,139 +46,9 @@ foreach ($udata as $prop => $value) {
 }
 
 // echo 'udata: <pre>' . print_r($udata, true) . '</pre>'; exit;
-// Connect to DB
-$db = db_connect() or die("Can't connect to database.");
+// Connect to DB - Has to be rebuilt if DB used
+// $db = db_connect() or die("Can't connect to database.");
 
-// Retrieve client list
-$clients = array();
-$query_clients = 'select pkClientID, ClientName, ClientShortName from tblClients where Active = 1';
-try {
-    $sth_clients = $db->prepare($query_clients);
-    $sth_clients->execute();
-    while ($row = $sth_clients->fetch(PDO::FETCH_ASSOC)) {
-        $clients[$row['pkClientID']] = $row;
-    }
-} catch (PDOException $e) {
-    http_response_code(500);
-    echo "\nPDO::errorInfo():\n";
-    print_r($db->errorInfo());
-}
-
-// Retrieve task list from server
-// $filtered_data contains data with some low-urgency tasks filtered out (see includes/defs.php)
-// echo '<pre>' . print_r($_SERVER, true) . '</pre>'; exit;
-
-$query = 'exec spoDashboard :user';
-
-
-
-$user_columns = array();
-
-try {
-    $sth = $db->prepare($query);
-    $sth->bindValue(':user', $user);
-    $sth->execute();
-
-    // If $dump_raw is set, just echo the raw output
-    if ($dump_raw) {
-        header('Content-Type: text/plain');
-        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-            print_r($row);
-            //exit;
-        }
-        exit;
-    }
-
-    while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-
-// Add client name
-        $clientName = '';
-        if (isset($clients[$row['fkClientID']])) {
-            $clientName = $clients[$row['fkClientID']]['ClientShortName'];
-        }
-
-        // Set a date which will not have any tasks after it
-        $keep_date = date_create('2099-01-01');
-
-// Map the tasks listed in $task_mapping into rows of the new array
-        // Set $eBlast to false if Net Counts has a value
-        $eBlast = stristr($row['Project'], 'Eblast') || stristr($row['Project'], 'E-blast');
-
-        foreach ($task_mapping_list as $map) {
-            $keep = array();
-
-            extract($map);
-
-            // Determine date - overwrite if new date is lower than existing
-            $dueDate = date_create($row[$key]);
-            $keep['DueDate'] = $dueDate->format('Y-m-d');
-
-            $date_diff = intval(date_diff($keep_date, $dueDate)->format("%R%a"));
-
-            // Skip completed tasks and those with a later due date
-            if ($row[$key . '_Comp'] !== '0' || $date_diff >= 0) {
-                continue;
-            }
-
-            // If $eBlast is false, do not display mail date
-            if ($value == 'Mail Date') {
-                if (!$eBlast) {
-                    continue;
-                } else {
-                    $value = '[EBLAST] Mail Date';
-                }
-            }
-
-            $keep_date = $dueDate;
-
-            // Status - based on relationship to today's date;
-            $keep['Status'] = get_status($dueDate);
-            // Suppress any tasks beyond radar
-            if ($keep['Status'] === 'Beyond Radar') {
-                continue;
-            }
-
-            $taskID = $row[$key . '_TaskID'];
-
-            // Set task type symbol
-            // See defs.php for sequence
-            $keep['Type'] = ($value == 'Net Counts' ? 4 : 1);
-
-            $keep['Docket'] = $row['pkDocketNum'];
-            $keep['TaskID'] = $taskID;
-            $keep['Client'] = $clientName;
-            $keep['CampaignName'] = $row['Project'];
-            $keep['TaskName'] = $value;
-
-            // Add user data
-            if (!empty($udata[$taskID])) {
-                foreach ($udata[$taskID] as $prop => $value) {
-                    if ($prop == 'keep') {
-                        continue;
-                    }
-                    $keep[$prop] = $value;
-                    if (!in_array($prop, $user_columns)) {
-                        array_push($user_columns, $prop);
-                    }
-                }
-                $udata[$taskID]['keep'] = true;
-            }
-
-            // If this task replaces an existing task (because it has an
-            // earlier due date) pop the last element off before adding
-            $lastElement = end($data);
-            if ($lastElement !== null && $lastElement['Docket'] == $keep['Docket']) {
-                array_pop($data);
-            }
-            array_push($data, $keep);
-            reset($data);
-        }
-    }
-} catch (PDOException $e) {
-    http_response_code(500);
-    echo "\nPDO::errorInfo():\n";
-    print_r($db->errorInfo());
-}
 
 // Create a separate columns list for easy reference
 $columns = array();
@@ -209,7 +75,6 @@ file_put_contents('udata/' . $user, $udata_string);
 // Output the data (for reading by AJAX)
 
 $results_array = array(
-    'clients' => $clients,
     'data' => $data,
     'columns' => $columns,
     'filtered_columns' => $filtered_columns
--- includes/defs.php
+++ includes/defs.php
@@ -5,14 +5,6 @@
  */
 
 
-define('DB_SERVER', 'BEA001\WAREHOUSE');
-define('DB_MASTER', 'master');
-define('DB_NAME', 'ProjectManagerSQL');
-
-define('TABLE_TASKS', 'tblCampaignTasks');
-define('TABLE_ROLES', 'tblCampaignRoles');
-define('TABLE_DOCKETS', 'tblDockets');
-
 define('MAX_DAYS_OUT', 30);
 
 define('SAVE_PATH', 'U:\Projects\dailytodo');
@@ -23,78 +15,43 @@ $columns_to_filter = array(
     'TaskID' => 1
 );
 
-$task_mapping_list = [
-    ['key' => 'DataPlanDue', 'value' => 'Data Plan'],
-    ['key' => 'ClientDataDue', 'value' => 'Client Data Due'],
-    ['key' => 'SampleDataDue', 'value' => 'IPR'],
-    ['key' => 'DataToPrime', 'value' => 'Instructions to Prime'],
-    ['key' => 'FinalDataDue', 'value' => 'Final Data Due'],
-    ['key' => 'NetCounts', 'value' => 'Net Counts'],
-    ['key' => 'MailDate', 'value' => 'Mail Date']
-    
-   ];
-
 $task_types = [
     [],
-    ['symbol' => 'D', 'type_name' => 'Data Work', 'in_cp' => true,
+    ['symbol' => 'A', 'type_name' => 'Task Type A', 
         'steps' => null],
-    ['symbol' => 'A', 'type_name' => 'Analysis', 'in_cp' => false,
+    ['symbol' => 'B', 'type_name' => 'Task Type A', 
         'steps' => [
             [
-                'name' => 'Email Summary Sent',
+                'name' => 'Step 1',
                 'days_out' => -6
                 ],
             [
-                'name' => '1st Review',
+                'name' => 'Step 2',
                 'days_out' => -5
                 ],
             [
-                'name' => '2nd Review',
+                'name' => 'Step 3',
                 'days_out' => -3
                 ],
             [
-                'name' => 'Write-up',
+                'name' => 'Step 4',
                 'days_out' => -1
                 ],
             [
-                'name' => 'Presentation',
+                'name' => 'Step 5',
                 'days_out' => 0
                 ],
         ]],
-    ['symbol' => 'R', 'type_name' => 'Results Reporting', 'in_cp' => false,
-                'steps' => [
-            [
-                'name' => 'Import & Update',
-                'days_out' => -3
-                ],
-            [
-                'name' => 'Reports',
-                'days_out' => -2
-                ],
-            [
-                'name' => 'Response Curves',
-                'days_out' => -1
-                ],
-            [
-                'name' => 'Issue Reports',
-                'days_out' => 0
-                ],
-        ]],
-    ['symbol' => 'N', 'type_name' => 'Net Counts', 'in_cp' => true,
-        'steps' => null],
-    ['symbol' => 'I', 'type_name' => 'Insights Work', 'in_cp' => false,
-        'steps' => null],
-    ['symbol' => 'O', 'type_name' => 'Other', 'in_cp' => false,
-        'steps' => null],
+
 ];
 
 // Establish the default display order of the columns
 $filtered_columns_default = [
     'Type',
     'TaskName',
-    'Docket',
-    'Client',
-    'CampaignName',
+    'SubprojectName',
+    'ContactName',
+    'ProjectName',
     'DueDate',
     'Status',
     'Priority',
